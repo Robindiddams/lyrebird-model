d6445a7a9bf3b20c41f51d61ac55e5a0fcc37138
Robin Diddams <robindiddams@gmail.com>
2019-04-04 22:39:28 -0400

    closer to working code😰

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM tensorflow/tensorflow:latest-gpu-py3
 RUN mkdir -p /home/lyrebird
 COPY src /home/lyrebird/app
 RUN pip install -r /home/lyrebird/app/LyreBird/requirements.txt
-RUN pip install flask flask_cors
+RUN pip install flask flask_cors boto
 WORKDIR /home/lyrebird/app
  
 EXPOSE 8080
--- src/app.py
+++ src/app.py
@@ -1,18 +1,29 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import json
+import os
+import boto3
 import LyreBird.Create_Music as model
+
+s3 = boto3.resource('s3')
 
 app = Flask(__name__)
 CORS(app)
  
 @app.route("/")
 def runModel():
-    model.generate()
-    data={}
-    data["success"]=True
-    return jsonify(data)
+    task_id = request.args.get('task_id')
+    newpid = os.fork()
+    if newpid == 0:
+        data={}
+        data["success"]=True
+        return jsonify(data)
+    else:
+        model.generate()
+        print("done")
+        for bucket in s3.buckets.all():
+            print(bucket.name)
+        return "OK" # this is a throwaway lol!
  
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
