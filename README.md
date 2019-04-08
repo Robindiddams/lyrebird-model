b7c815d4361913134c1bf1f0ba5bf302f61b0480
Robin Diddams <robindiddams@gmail.com>
2019-04-08 11:26:57 -0400

    done!

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ the container for running the lyrebird music generation model
 ```
 docker build --build-arg key_id=$AWS_ACCESS_KEY_ID --build-arg access_key=$AWS_SECRET_ACCESS_KEY -t lyrebird-model:1.0.1 .
 ```
-**NOTE:** its **not** a good idea to put the credentials in at build time like this, and you should **never** pass root/admin credentials into your microservices. See [here](https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-iam-roles.html) for how to properly auth in a task.
+**NOTE:** its **not** a good idea to put the credentials in at build time like this, and you should **never** pass root/admin credentials into your production microservices. See [here](https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-iam-roles.html) for how to properly auth in a task.
 
 push it to your ecs cluster and then hit it like this:
 ```
--- src/app.py
+++ src/app.py
@@ -8,23 +8,22 @@ import subprocess
 import boto3
 s3 = boto3.client('s3')
 BUCKET = "lyrebird-sounds"
-print(os.environ["AWS_ACCESS_KEY_ID"])
-print(os.environ["AWS_SECRET_ACCESS_KEY"])
 app = Flask(__name__)
 CORS(app)
  
 @app.route("/")
 def runModel():
     task_id = request.args.get('task_id')
-    if (task_id is None):
+    seed = request.args.get('seed')
+    if ( (task_id is None) or (seed is None) ):
         data={}
         data["success"]=False
-        data["message"]="missing task id"
+        data["message"]="missing task id or seed"
         return jsonify(data)
     filepath = task_id + ".mid"
     cpid = os.fork()
     if cpid is 0:
-        subprocess.run(["python3", "LyreBird/Create_Music.py", task_id])
+        subprocess.run(["python3", "LyreBird/Create_Music.py", task_id, seed])
         s3.upload_file(filepath, BUCKET, "task_"+task_id)
         os.remove(filepath)
         return "OK"
